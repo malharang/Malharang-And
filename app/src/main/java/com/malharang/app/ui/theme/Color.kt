e20d288a1a0c5f2ad1a28f24300477b8bdc9bfca
seungjunGong <jjw5741@naver.com>
2025-05-21 14:01:07 +0900

    init/#1: Theme Color 및 Type 정의
    - Theme 수정
    - 사용자 정의 Color 및 Type 추가

--- app/src/main/java/com/malharang/app/ui/theme/Color.kt
+++ app/src/main/java/com/malharang/app/ui/theme/Color.kt
@@ -1,11 +1,32 @@
 package com.malharang.app.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Black
+val Black = Color(0xFF000000) // 100%
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// White
+val White = Color(0xFFFFFFFF)
+
+// Android
+val AndSysGray = Color(0xFF858585)
+val AndSysWhite = Color(0xFFF6F6F8)
+
+@Immutable
+data class MalHaRangColors(
+    val black: Color,
+    val white: Color,
+    val andSysGray: Color,
+    val andSysWhite: Color,
+)
+
+val defaultMalHaRangColors = MalHaRangColors(
+    black = Black,
+    white = White,
+    andSysGray = AndSysGray,
+    andSysWhite = AndSysWhite,
+)
+
+val LocalMalHaRangColorsProvider = staticCompositionLocalOf { defaultMalHaRangColors }
