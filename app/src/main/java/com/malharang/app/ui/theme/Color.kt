a1a9e047ff693e8c21aa229bd3ea9943dd4b5c07
seungjun <jjw5741@naver.com>
2025-06-02 23:35:05 +0900

    feat/#4: 홈 화면 미션 카드 다이얼로그 구현

--- app/src/main/java/com/malharang/app/ui/theme/Color.kt
+++ app/src/main/java/com/malharang/app/ui/theme/Color.kt
@@ -9,9 +9,11 @@ val Black = Color(0xFF000000) // 100%
 
 // White
 val White = Color(0xFFFFFFFF)
+val White20 = Color(0x33FFFFFF)
 
 // Gray
 val Gray = Color(0xFFF5F0E5)
+val GrayDark = Color(0xFF8E8E93)
 
 // Green
 val GreenLight = Color(0xFFD3EEDD)
@@ -27,6 +29,7 @@ data class MalHaRangColors(
     val black: Color,
     val white: Color,
     val gray: Color,
+    val grayDark: Color,
     val greenLight: Color,
     val greenDark: Color,
     val green: Color,
@@ -38,6 +41,7 @@ val defaultMalHaRangColors = MalHaRangColors(
     black = Black,
     white = White,
     gray = Gray,
+    grayDark = GrayDark,
     greenLight = GreenLight,
     greenDark = GreenDark,
     green = Green,
