beb1868cbb65bf5edbf51565cef7c69ab05e5d9a
MinGi <zxc12250@naver.com>
2025-06-04 00:01:15 +0900

    feat/#2: 공통 MissionScreen.kt ktlint 실행 및 UI 구현

--- app/src/main/java/com/malharang/app/presentation/screen/mission/MissionScreen.kt
+++ app/src/main/java/com/malharang/app/presentation/screen/mission/MissionScreen.kt
@@ -91,15 +91,15 @@ private fun MissionScreen(padding: PaddingValues) {
                 fontWeight = FontWeight.Bold,
                 fontSize = 18.sp
             )
-            DropdownItems("Review Mission")
-            DropdownItem("Export Sentences")
+            DropdownMissions("Review Mission")
+            SentenceItem("Export Sentences")
         }
     }
 }
 data class MissionData(val title: String, val description: String)
 
 @Composable
-fun DropdownItems(title: String) {
+fun DropdownMissions(title: String) {
     var expanded by remember { mutableStateOf(false) }
 
     val completedMissions = listOf(
@@ -164,7 +164,7 @@ fun DropdownItems(title: String) {
 }
 
 @Composable
-fun DropdownItem(title: String) {
+fun SentenceItem(title: String) {
     var expanded by remember { mutableStateOf(false) }
 
     val completedMissions = listOf(
