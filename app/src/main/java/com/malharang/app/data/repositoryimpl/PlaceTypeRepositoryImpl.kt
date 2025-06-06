5252ba2443fa11d06ed7a9bf22afa11dd7951085
seungjun <jjw5741@naver.com>
2025-06-06 11:55:52 +0900

    chore/#16: 필요 없는 함수 제거

--- app/src/main/java/com/malharang/app/data/repositoryimpl/PlaceTypeRepositoryImpl.kt
+++ app/src/main/java/com/malharang/app/data/repositoryimpl/PlaceTypeRepositoryImpl.kt
@@ -1,10 +1,8 @@
 package com.malharang.app.data.repositoryimpl
 
 import android.content.Context
-import com.malharang.app.R
 import com.malharang.app.domain.repository.PlaceTypeRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
-import org.json.JSONArray
 import javax.inject.Inject
 
 class PlaceTypeRepositoryImpl @Inject constructor(
@@ -12,14 +10,6 @@ class PlaceTypeRepositoryImpl @Inject constructor(
 ) : PlaceTypeRepository {
     private val recentKey = "recent_place_types"
     private val prefs by lazy { context.getSharedPreferences("place_type", Context.MODE_PRIVATE) }
-    private val allTypes by lazy { loadTypesFromRaw() }
-
-    private fun loadTypesFromRaw(): List<String> {
-        val inputStream = context.resources.openRawResource(R.raw.place_types)
-        val json = inputStream.bufferedReader().use { it.readText() }
-        val array = JSONArray(json)
-        return List(array.length()) { array.getString(it) }
-    }
 
     override suspend fun getRecentPlaceTypes(): List<String> =
         prefs.getStringSet(recentKey, emptySet())?.toList() ?: emptyList()
