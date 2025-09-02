7eacf372e469b42056fedff94b987c2b67d785b0
seungjun <jjw5741@naver.com>
2025-09-02 12:30:59 +0900

    add/#31: 공유 뷰모델 추가

---
+++ app/src/main/java/com/malharang/app/core/util/NavBackStackEntryExt.kt
@@ -0,0 +1,23 @@
+package com.malharang.app.core.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+
+/**
+ *  공유 뷰모델 을 위한 확장 함수
+ */
+@Composable
+inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(
+    navController: NavHostController,
+): T {
+    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return hiltViewModel(parentEntry)
+}
